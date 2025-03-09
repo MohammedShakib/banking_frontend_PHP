e170e939153f405b310c6f2ec8f3a1dfdd362ebc
Mohammed Shakib <nayemshakib2018@gmail.com>
2025-03-09 19:23:40 +0600

    login page with hyperlink

--- db_config.php
+++ db_config.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "ubank";
+$servername = "localhost"; // Change if your database host is different
+$username = "root"; // Default XAMPP MySQL username
+$password = ""; // Default is empty in XAMPP
+$dbname = "ubank"; // Change this to your actual database name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
